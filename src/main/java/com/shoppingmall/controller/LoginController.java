129e7aef879a46d1d5bff88cc9b81495cbd9716b
HyungTaeKim8905 <75460537+HyungTaeKim8905@users.noreply.github.com>
2021-01-15 13:40:11 +0900

    로그인 로직 추가

---
+++ src/main/java/com/shoppingmall/controller/LoginController.java
@@ -0,0 +1,48 @@
+package com.shoppingmall.controller;
+
+import java.util.Locale;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.shoppingmall.service.LoginService;
+import com.shoppingmall.vo.LoginVO;
+import com.shoppingmall.vo.MembersVO;
+
+@Controller
+public class LoginController {
+	
+	@Inject
+	LoginService loginService;
+	
+	
+	//로그인
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String Login(Locale locale, Model model, LoginVO vo, HttpServletRequest request) throws Exception {
+		
+		//비밀번호 암호화
+		vo.setPassword(MembersVO.bytesToHex2(MembersVO.sha256(vo.getPassword())));
+		
+		LoginVO result = loginService.Login(vo);
+		if(vo.getMemberid() == result.getMemberid()) {
+			HttpSession session = request.getSession();
+			session.setAttribute("memberid", result.getMemberid());
+		}
+		
+		return "/home";
+	}
+	
+
+	//로그아웃
+	@RequestMapping(value = "/logout", method = RequestMethod.POST)
+	public String Logout(Locale locale, Model model) throws Exception {
+		
+		return "/home";
+	}
+}

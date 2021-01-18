f531a31a9f7fa756f738c757a613dc1b8d848f78
HyungTaeKim8905 <75460537+HyungTaeKim8905@users.noreply.github.com>
2021-01-18 14:53:52 +0900

    수정

--- src/main/java/com/shoppingmall/service/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.shoppingmall.service;
-
-import java.util.List;
-
-import com.shoppingmall.vo.testVO;
-
-public interface TestService {
-	public List<testVO> selectTest() throws Exception;
-}

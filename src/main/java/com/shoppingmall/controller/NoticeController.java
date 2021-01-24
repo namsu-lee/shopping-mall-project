bb2d3c02ecdf91ac9d455e5c5243c59ad49cfc74
HyungTaeKim8905 <75460537+HyungTaeKim8905@users.noreply.github.com>
2021-01-25 00:14:44 +0900

    수정

---
+++ src/main/java/com/shoppingmall/controller/NoticeController.java
@@ -0,0 +1,15 @@
+package com.shoppingmall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class NoticeController {
+
+	
+	@RequestMapping(value = "/notice", method = RequestMethod.GET)
+	public String go() {
+		return "/notice";
+	}
+}

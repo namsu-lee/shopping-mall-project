73ffd3d31ffde71d3afeb3df1aec2a6637f1350e
namsu <dlskatnekrh@gmail.com>
2021-01-27 16:53:01 +0900

    댓글 Ajax 입력, 삭제 구현

--- src/main/java/com/shoppingmall/controller/ReplyController.java
+++ src/main/java/com/shoppingmall/controller/ReplyController.java
@@ -1,74 +1,57 @@
 package com.shoppingmall.controller;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 import javax.inject.Inject;
 
-import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shoppingmall.service.NoticeService;
 import com.shoppingmall.service.ReplyService;
-import com.shoppingmall.vo.BoardVO;
-import com.shoppingmall.vo.NoticeVO;
 import com.shoppingmall.vo.ReplyVO;
 
 @RestController
-@Controller
 public class ReplyController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
 	@Inject
 	private ReplyService reply;
 	
 	@Inject
 	NoticeService noticeService;
 	
-	//댓글 작성
-	@RequestMapping(value = "/board/{cateid}/{b_num}/wrotereply")
-	public String WroteReply(@PathVariable Integer cateid, @PathVariable Integer b_num, ReplyVO vo, Locale locale, Model model) throws Exception {
-
+	//댓글 불러오기
+	@RequestMapping(value = "/GetReply")
+	public List<ReplyVO> GetReply(int b_num) throws Exception {
+		return reply.GetReply(b_num);
+	}
+	
+	@RequestMapping(value = "/wrotereply")
+	public Map<String, Object> WroteReply(@RequestBody ReplyVO vo, 
+			Locale locale, Model model) throws Exception {
+		
+		Map<String, Object> result = new HashMap<>();
+		
+		try {
 		reply.WroteReply(vo);
-		System.out.println(vo.getReplynum());
-		
-		BoardVO boardVO = new BoardVO();
-		boardVO.setCateid(cateid);
-		boardVO.setB_num(b_num);
-		
-		//게시글은 쓴 사람의 아이디 구해야 한다.
-		Map<String, String> map = noticeService.getBoardMemberid(boardVO);
-		System.out.println("게시글은 쓴 사람의 아이디 == " + map.get("memberid"));
-		//방금 작성한 댓글 번호를 가져옴
-		//int num = noticeService.getReplyNo();
-		//System.out.println("방금 작성한 댓글 번호 == " + num);
-		
-		
-		//알람 로직
-		//가지고 올것 -> b_num : 게시글 번호, 댓글 번호
-		NoticeVO noticeVO = new NoticeVO();
-		noticeVO.setCateid(cateid); 			//카테고리 아이디
-		noticeVO.setBoard_no(b_num);			//게시글 번호
-		noticeVO.setReply_no(vo.getReplynum());//댓글 번호
-		noticeVO.setMemberid(map.get("memberid"));		//게시글을 쓴 사람의 id(알람 받을 사람)
-		noticeVO.setAnother_memberid(vo.getMemberid());//댓글을 쓴 사람의 아이디
-		
-		
-		
-		//notice 테이블에 저장 (확인차 찍어봅니다......)
-		int result = noticeService.NoticeList(noticeVO);
-		if(result == 1) {
-			System.out.println("잘 저장이 되었다.");
+		result.put("status", "OK");
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.put("status", "False");
 		}
-		else {
-			System.out.println("잘 저장이 안되었다.@@@@@@");
-		}
-		
-		
-		
-		
-		return "redirect:/board/{cateid}/{b_num}";
+		return result;
 	}
 	//댓글 수정
 	@RequestMapping(value = "/board/{cateid}/{b_num}/{replynum}/updatedreply")
@@ -80,14 +63,21 @@ public class ReplyController {
 	}
 	
 	//댓글 삭제
+	@RequestMapping(value = "/deletereply")
+	@ResponseBody
+	public Map<String, Object> DeleteReply( Integer replynum, Locale locale, Model model) throws Exception {
+		Map<String, Object> result = new HashMap<>();
+		
+		try {
+			reply.DeleteNotice(replynum);
+			reply.DeleteReply(replynum);
+			result.put("status", "OK");
 	
-	@RequestMapping(value = "/board/{cateid}/{b_num}/{replynum}/deletereply")
-	public String DeleteReply(@PathVariable Integer replynum, @PathVariable Integer b_num, @PathVariable Integer cateid, Locale locale, Model model) throws Exception {
-		
-		reply.DeleteNotice(replynum);
-		reply.DeleteReply(replynum);
-		
-		return "redirect:/board/{cateid}/{b_num}";
+			} catch (Exception e) {
+			e.printStackTrace();
+			result.put("status", "False");
+			}
+			return result;
 	}
 	
 }
